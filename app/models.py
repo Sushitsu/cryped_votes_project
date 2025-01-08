f2fc0a53586c3e6c4ac7e5a24cded1702862ffb1
Matéo DE MUYNCK <mdemuynck@dream-on.tech>
2025-01-08 09:48:18 +0100

    Config et models adaptés  la bdd

--- app/models.py
+++ app/models.py
@@ -2,8 +2,7 @@ from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    username = db.Column(db.String(80), primary_key=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
 
     def set_password(self, password):
